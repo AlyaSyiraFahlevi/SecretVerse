ca43dbd7860243657c31f36b9b236f73d95a3566
DiangAnggunPratiwi <164192111+DiangAnggunPratiwi@users.noreply.github.com>
2024-03-28 09:19:17 +0700

    Update create_link.php

--- api/create_link.php
+++ api/create_link.php
@@ -3,5 +3,6 @@ if (isset($_GET['id_link'])) {
     $_id_link = $_GET['id_link'];
 
     $database = file_get_contents("database/data.txt");
-    $json_obj = json_decode
+    $json_obj = json_decode($database);
+    if (isset($json_obj-
 }
