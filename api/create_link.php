686f3f14010d9a2f49d063a0eada9650d4bc398f
DiangAnggunPratiwi <164192111+DiangAnggunPratiwi@users.noreply.github.com>
2024-03-28 09:40:54 +0700

    Update create_link.php

--- api/create_link.php
+++ api/create_link.php
@@ -4,5 +4,22 @@ if (isset($_GET['id_link'])) {
 
     $database = file_get_contents("database/data.txt");
     $json_obj = json_decode($database);
-    if (isset($json_obj-
+    if (isset($json_obj->$id_link)){
+
+        $id_pesan = "idpesan".uniqid();
+        if(!($json_obj->$id_link->pesan == null)){
+            echo "data sudah ada komentar sebelumnya";
+
+        }else{
+            echo "Data belum ada komentar sebelumnya";
+
+            $json_obj->$id_link->pesan = [["id_pesan"=>"$id_pesan"]];
+        }
+
+    }else{
+        echo json_encode(["status"=>false]);
+    }
+
+}else{
+    echo json_encode(["status"=>false]);
 }
